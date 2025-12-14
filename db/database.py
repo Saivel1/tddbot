b68af02adbaadbe42cd67dfa670b5ef27bf5469a
Saivel1 <saivel.mezencev1@gmail.com>
2025-12-14 15:17:10 +0300

    db

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from config import settings
 
 engine = create_async_engine(
-    url=settings.DATABASE_URL_aiosqlite
+    url=settings.DATABASE_URL
 )
 
 async_session_maker = async_sessionmaker(
