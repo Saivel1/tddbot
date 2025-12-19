5a0513362582fea022b0a673eaec22fcce10a76c
Saivel1 <saivel.mezencev1@gmail.com>
2025-12-19 18:53:35 +0300

    marz

--- app/main.py
+++ app/main.py
@@ -187,6 +187,8 @@ async def webhook_marz(
     data_str = json.dumps(data, ensure_ascii=False)
 
     first_item = data[0]
+    logger.info(first_item)
+    logger.info(data)
 
     username  = first_item.get('username')
     action    = first_item.get('action')
