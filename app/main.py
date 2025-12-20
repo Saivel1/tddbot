f18503b6f8cd5783cf54710901ec36460a3405dc
Saivel1 <saivel.mezencev1@gmail.com>
2025-12-20 12:52:56 +0300

    try

--- app/main.py
+++ app/main.py
@@ -81,10 +81,10 @@ async def lifespan(app: Litestar):
     print("✅ Database tables created")
 
     # Webhook setup
-    await bot.delete_webhook()
-    webhook_url = f"{s.WEBHOOK_URL}"
-    await bot.set_webhook(url=webhook_url, drop_pending_updates=True)
-    print(f"✅ Webhook установлен: {webhook_url}")
+    # await bot.delete_webhook()
+    # webhook_url = f"{s.WEBHOOK_URL}"
+    # await bot.set_webhook(url=webhook_url, drop_pending_updates=True)
+    # print(f"✅ Webhook установлен: {webhook_url}")
 
     # ✅ Создаём долгоживущую сессию для воркеров
     worker_session = async_session_maker()
@@ -121,8 +121,8 @@ async def lifespan(app: Litestar):
         await conn.run_sync(Base.metadata.drop_all)
     print("✅ Database tables dropped")
     
-    await bot.delete_webhook()
-    await bot.session.close()
+    # await bot.delete_webhook()
+    # await bot.session.close()
 
 
 async def provide_redis() -> Redis: #type: ignore
