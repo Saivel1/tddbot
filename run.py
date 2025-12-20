b708de8f234cde855360dbdf88de41851298e214
Saivel1 <saivel.mezencev1@gmail.com>
2025-12-20 12:47:59 +0300

    выф

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         target="app.main:app",
         address="127.0.0.1",
         port=8000,
-        workers=2,
+        workers=1,
         loop=Loops.asyncio,
         log_enabled=True,
         interface=Interfaces.ASGI,
